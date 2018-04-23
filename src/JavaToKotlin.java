2ae0f8d4e985c904d188fe8f7772020609645f69
mjesar <mohammadalijaisar@gmail.com>
2018-04-23 21:36:43 +0500

    kotlin

---
+++ src/JavaToKotlin.java
@@ -0,0 +1,19 @@
+public class JavaToKotlin {
+
+    int rollNo = 0;
+    String name= "";
+
+    public static void main(String[] args){
+
+       System.out.print( Kotlin_with_JavaKt.addd(1,3));
+
+    }
+
+    public void show(){
+
+        System.out.println("RollNo: "+rollNo);
+        System.out.println("Name: "+ name);
+    }
+
+
+}
---
+++ src/Kotlin with Java.kt
@@ -0,0 +1,25 @@
+
+
+@file:JvmName("Kotlin_with_JavaKt")
+
+
+
+        fun main(args:Array<String>){
+
+            var a = 200
+
+            //conversion datatypes
+
+            //Kotlin Print
+            println("kotlin")
+            println("34".toInt())
+
+            //In Java
+            System.out.println("Java")
+            System.out.println(Integer.parseInt("65"))
+
+
+
+
+}
+        fun addd(a: Int, b: Int): Int = a+b

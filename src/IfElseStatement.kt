3cd1dbcd947a203da79781796581a916983b937e
mjesar <mohammadalijaisar@gmail.com>
2018-04-23 12:58:40 +0500

    kotlin

---
+++ src/IfElseStatement.kt
@@ -0,0 +1,17 @@
+
+
+
+
+fun main(args: Array<String>){
+
+    var a = 2
+    var b = 4
+
+    if (a==b){
+        println("A is equal to B")
+    }else{
+        println("A is not equal to B")
+    }
+
+
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,36 @@
 
 
 fun main(args : Array<String>){
-    println("Hello World")
+
+    //String
+    var name: String ="Ali"
+
+
+    //constant
+    val constant = 12
+
+    //integer
+    var num = 23
+
+    //print
+    println("Name "+name+num)
+
+    // print max and min value
+    println("Max value: "+Int.MAX_VALUE)
+    println("Min value: "+ Int.MIN_VALUE)
+
+    // ARRAY
+
+
+    var myArray = arrayOf("Ali",1,3,4,53,23,33333,2222222222,33.00000000)
+
+    // perticular type of array
+
+    var intArray : Array<Int> = arrayOf(1,2,3,4,5,6)
+    var stringArray : Array<String> = arrayOf("sdffds","dssdfsdf")
+
+    for(x in myArray){
+        println(x)
+    }
+
 }

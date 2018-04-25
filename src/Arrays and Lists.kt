238589a7ae7d2d74ce10fe379e43ad3b74c4a104
mjesar <mohammadalijaisar@gmail.com>
2018-04-25 16:39:17 +0500

    kotlin complete basics

--- src/Arrays and Lists.kt
+++ src/Arrays and Lists.kt
@@ -7,10 +7,31 @@
 
             var name = arrayOf(1,2,3,4,4,5,3)
             for (i in name){
-                println(i)
+                print(i)
 
             }
 
+
+
+            /* HIGHER ORDER FUNCTIONS */
+
+            var rolls = intArrayOf(1,2,3,4,5,6,7)
+
+            // get even  numbers from array
+            var even= rolls.filter { it%2==0 }
+
+            //twice multiply numbers of array
+            var twice = rolls.map { it*2 }
+
+            rolls.forEach { print(it) }
+
+            println("\nEven Numbers")
+
+            even.forEach { print(it) }
+
+            println("\nTwice Numbers")
+
+            twice.forEach { print(it) }
 
 
 
